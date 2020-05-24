14629b6a412a85766c989262de88d9aa29bf066d
han1475 <me@han1475.com>
2020-05-24 23:50:10 +0800

    remove unix build config in build script

--- build.sh
+++ build.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
+platform="windows"
+outfile="lite.exe"
+compiler="x86_64-w64-mingw32-gcc"
 cflags="-Wall -O3 -g -std=gnu11 -Isrc -DLUA_USE_POPEN"
+cflags="$cflags -Iwinlib/SDL2-2.0.10/x86_64-w64-mingw32/include"
 lflags="-lSDL2 -lm"
+lflags="$lflags -Lwinlib/SDL2-2.0.10/x86_64-w64-mingw32/lib"
+lflags="-lmingw32 -lSDL2main $lflags -mwindows -o $outfile res.res"
 
-if [[ $* == *windows* ]]; then
-  platform="windows"
-  outfile="lite.exe"
-  compiler="x86_64-w64-mingw32-gcc"
-  cflags="$cflags -Iwinlib/SDL2-2.0.10/x86_64-w64-mingw32/include"
-  lflags="$lflags -Lwinlib/SDL2-2.0.10/x86_64-w64-mingw32/lib"
-  lflags="-lmingw32 -lSDL2main $lflags -mwindows -o $outfile res.res"
-  windres res.rc -O coff -o res.res
-else
-  platform="unix"
-  outfile="lite"
-  compiler="gcc"
-  lflags="$lflags -o $outfile"
-fi
-
+windres res.rc -O coff -o res.res
 if command -v ccache >/dev/null; then
   compiler="ccache $compiler"
 fi
-
 
 echo "compiling ($platform)..."
 for f in `find src -name "*.c"`; do
